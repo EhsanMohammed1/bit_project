1da4decec49f556a8cb9cf3eaae41ffc876ef597
wecho12 <ehsankurd286@gmail.com>
2023-04-04 03:34:26 +0300

    Product component done

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,54 +2,54 @@ import React from "react";
 
 const Hero = () => {
   return (
-    
-     <section class=" bg-violet-900  grid max-w-screen-l mx-auto lg:gap-8 px-4  sm:px-6 md:px-8 lg:px-10 lg:py-20 lg:grid-cols-12  ">
-  <div class="mr-auto place-self-center lg:col-span-7 ">
-    <h1 class="max-w-7xl mb-4 px-20 text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold tracking-tight text-white ">
-      Find The Perfect
-      Design <br  />
-      For Your Devices
-      <div class="relative w-full sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-8/12 pt-8 sm:pt-12 ">
-        <div class="absolute flex items-center pl-3 pointer pt-3 ">
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-gray-900 dark:text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          class="block w-full p-3 md:p-4 pl-10 text-sm md:text-base text-gray-900"
-          placeholder="Search brand, category, product name ..."
-        />
-        <button
-          type="submit"
-          class="text-white absolute right-2.5 bottom-2.5 bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm md:text-base px-4 py-2 dark:bg-bray-600 dark:bray:bg-blue-700 dark:focus:ring-violet-800"
-        >
-          Search
-        </button>
+
+    <section class=" bg-violet-900  grid max-w-screen-l mx-auto lg:gap-8 px-4  sm:px-6 md:px-8 lg:px-10 lg:py-20 lg:grid-cols-12  ">
+      <div class="mr-auto place-self-center lg:col-span-7 ">
+        <h1 class="max-w-7xl mb-4 px-20 text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold tracking-tight text-white ">
+          Find The Perfect
+          Design <br />
+          For Your Devices
+          <div class="relative w-full sm:w-9/12 md:w-8/12 lg:w-7/12 xl:w-8/12 pt-8 sm:pt-12 ">
+            <div class="absolute flex items-center justify-center pl-3 pointer pt-4 ">
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 self-center   text-gray-900 dark:text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              class="block indent-5 w-full p-3 md:p-4 pl-10 text-sm md:text-base text-gray-900"
+              placeholder="Search brand, category, product name ..."
+            />
+            <button
+              type="submit"
+              class="text-white absolute right-2.5 bottom-2.5 bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-lg text-sm md:text-base px-4 py-2 dark:bg-bray-600 dark:bray:bg-blue-700 dark:focus:ring-violet-800"
+            >
+              Search
+            </button>
+          </div>
+        </h1>
       </div>
-    </h1>
-  </div>
-  <div class="lg:mt-0 px-20 lg:col-span-5 flex justify-center">
-    <img
-      src="https://static.vecteezy.com/system/resources/previews/012/981/082/original/wireless-headphones-side-view-white-icon-on-a-transparent-background-3d-rendering-png.png"
-      alt="hero"
-      class="w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-none"
-    />
-  </div>
-</section>
-   
+      <div class="lg:mt-0 px-20 lg:col-span-5 flex justify-center">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/012/981/082/original/wireless-headphones-side-view-white-icon-on-a-transparent-background-3d-rendering-png.png"
+          alt="hero"
+          class="w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-none"
+        />
+      </div>
+    </section>
+
   );
 };
 
--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,92 +1,107 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 
 import React from 'react'
 
 const Products = () => {
   return (
-    <>
-      <div className="flex flex-col md:flex-row justify-center items-center gap-8 px-8 py-8  ">
-        <a
-          href="#"
-          className="relative rounded-lg overflow-hidden border border-red-500  " >
-          <img
-            alt="Card image"
-            className=" "
-            src="https://freepngimg.com/save/33095-phone-case-transparent/680x743"
-          />
-          <div className="absolute bottom-0 w-full h-16 bg-white opacity-75 flex items-center justify-center">
-            <h2 className="text-gray-900 font-bold text-lg">Card Header</h2>
-          </div>
-        </a>
-        <a
-          href="#"
-          className="relative rounded-lg overflow-hidden border border-red-500 mb-4 md:mb-0 md:mr-4"
+    <section
+      className="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 tails-selected-element px-52 "
+      contentEditable="true"
+    >
+
+      <div
+        className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl cursor-pointer border border-violet-600"
+        data-rounded="rounded-xl"
+        data-rounded-max="rounded-full"
+      >
+        <div
+          className="p-3 text-white"
+
         >
-          <img
-            alt="Card image"
-            className="object-cover object-center w-full h-48 block"
-            src="https://via.placeholder.com/640x360.png?text=Card+Image"
-          />
-          <div className="absolute bottom-0 w-full h-16 bg-white opacity-75 flex items-center justify-center">
-            <h2 className="text-gray-900 font-bold text-lg">Card Header</h2>
-          </div>
-        </a>
-        <a
-          href="#"
-          className="relative rounded-lg overflow-hidden border border-red-500 mb-4 md:mb-0"
-        >
-          <img
-            alt="Card image"
-            className="object-cover object-center w-full h-48 block"
-            src="https://via.placeholder.com/640x360.png?text=Card+Image"
-          />
-          <div className="absolute bottom-0 w-full h-16 bg-white opacity-75 flex items-center justify-center">
-            <h2 className="text-gray-900 font-bold text-lg">Card Header</h2>
-          </div>
-        </a>
+          <img className='w-50 h-56 ' src="https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2 " alt="" />
+
+        </div>
+        <h4 className="text-xl font-medium text-black">Case &Protector</h4>
+
       </div>
-      <div className="flex flex-col md:flex-row justify-center items-center gap-8">
-        <a
-          href="#"
-          className="relative rounded-lg overflow-hidden border border-red-500 mt-4 md:mt-0 md:mr-4"
+      <div
+        className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl cursor-pointer border border-violet-600"
+        data-rounded="rounded-xl"
+        data-rounded-max="rounded-full"
+      >
+        <div
+          className="p-3 text-white"
+
         >
-          <img
-            alt="Card image"
-            className="object-cover object-center w-full h-48 block"
-            src="https://via.placeholder.com/640x360.png?text=Card+Image"
-          />
-          <div className="absolute bottom-0 w-full h-16 bg-white opacity-75 flex items-center justify-center">
-            <h2 className="text-gray-900 font-bold text-lg">Card Header</h2>
-          </div>
-        </a>
-        <a
-          href="#"
-          className="relative rounded-lg overflow-hidden border border-red-500 mt-4 md:mt-0 md:mr-4"
-        >
-          <img
-            alt="Card image"
-            className="object-cover object-center w-full h-48 block"
-            src="https://via.placeholder.com/640x360.png?text=Card+Image"
-          />
-          <div className="absolute bottom-0 w-full h-16 bg-white opacity-75 flex items-center justify-center">
-            <h2 className="text-gray-900 font-bold text-lg">Card Header</h2>
-          </div>
-        </a>
-        <a
-          href="#"
-          className="relative rounded-lg overflow-hidden border border-red-500 mt-4 md:mt-0"
-        >
-          <img
-            alt="Card image"
-            className="object-cover object-center w-full h-48 block"
-            src="https://via.placeholder.com/640x360.png?text=Card+Image"
-          />
-          <div className="absolute bottom-0 w-full h-16 bg-white opacity-75 flex items-center justify-center">
-            <h2 className="text-gray-900 font-bold text-lg">Card Header</h2>
-          </div>
-        </a>
+          <img className='w-50 h-56' src="https://www.pngall.com/wp-content/uploads/5/Gaming-Headset-PNG-Free-Image-1.png" alt="" />
+
+        </div>
+        <h4 className="text-xl font-medium text-black">Headset</h4>
+
       </div>
-    </>
+      <div
+        className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 border-red-700 rounded-sm sm:rounded-xl cursor-pointer border border-violet-600"
+        data-rounded="rounded-xl"
+        data-rounded-max="rounded-full"
+      >
+        <div
+
+        >
+          <img className='w-50 h-56' src="https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/pdp/headphones/freebuds-pro-2/imgs/huawei-freebuds-pro-2-id-color2-1.png" alt="" />
+
+        </div>
+        <h4 className="text-xl font-medium text-black">EarPhone</h4>
+
+      </div>
+      <div
+        className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl cursor-pointer border border-violet-600"
+        data-rounded="rounded-xl"
+        data-rounded-max="rounded-full"
+      >
+        <div
+          className="p-3 text-white"
+          data-primary="blue-500"
+          data-rounded="rounded-full"
+        >
+          <img className='w-50 h-56' src="https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP860/series7-480.png " alt="" />
+
+        </div>
+        <h4 className="text-xl font-medium text-gray-700">Smart Whatch</h4>
+
+      </div>
+      <div
+        className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl cursor-pointer border border-violet-600"
+        data-rounded="rounded-xl"
+        data-rounded-max="rounded-full"
+      >
+        <div
+          className="p-3 text-white"
+          data-primary="blue-500"
+          data-rounded="rounded-full"
+        >
+          <img className='w-50 h-56' src="https://www.pngarts.com/files/9/Type-C-Charger-Adapter-PNG-Photo.png" alt="" />
+
+        </div>
+        <h4 className="text-xl font-medium text-gray-700">Charging</h4>
+
+      </div>
+      <div
+        className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl cursor-pointer border border-violet-600"
+        data-rounded="rounded-xl"
+        data-rounded-max="rounded-full"
+      >
+        <div
+          className="p-3 text-white"
+          data-primary="blue-500"
+          data-rounded="rounded-full"
+        >
+          <img className='w-50 h-56' src="https://consumer.huawei.com/content/dam/huawei-cbg-site/greate-china/cn/mkt/pdp/speakers/sound-x-2021/specs/sound-x-2021-specs.png" alt="" />
+
+        </div>
+        <h4 className="text-xl font-medium text-gray-700">Speake & Stand .... </h4>
+
+      </div>
+    </section>
+
 
 
 
