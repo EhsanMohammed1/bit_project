290b7cc8edefad459005e16d3f7d17d51d4ab212
wecho12 <ehsankurd286@gmail.com>
2023-04-08 02:05:14 +0300

    mapping from objects

--- src/component/Items.jsx
+++ src/component/Items.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
-import { Data } from "../data"
-const Items = (item) => {
-  return (
-    <>
-      <div className='flex pt-20 py-10 justify-center'>
-        <h1 className='text-black underline text-2xl font-extrabold tracking-wide uppercase'>Products</h1>
-      </div>
-      <section
+import { PRODUCT } from "../data"
 
-        className="grid grid-cols-4 gap-8 sm:grid-cols-8 lg:grid-cols-12 tails-selected-element px-52 "
-        contentEditable="true"
-      >
 
+const Items = () => {
+  const cardItem = (item) => {
+    return (
+      <div>
         <div
-          className="flex flex-col items-center justify-between col-span-4 px-8 py-8 space-y-4 bg-gray-100 sm:rounded-xl cursor-pointer border border-violet-600"
-          data-rounded="rounded-xl"
-          data-rounded-max="rounded-full"
-        >
+          className="flex flex-col items-center justify-between  bg-gray-100 sm:rounded-[8px] cursor-pointer border border-violet-600 ">
           <div
-            className="p-3 text-white"
-
-          >
-            <img className='w-50 h-56 ' src="https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2 " alt="" />
+            className=" text-white" >
+            <img src={item.img} alt="card" />
 
           </div>
-          <h4 className="text-xl font-medium text-black">Case &Protector</h4>
+
+          <h4 className="text-xl font-[400] text-black">{item.brand}</h4>
+          <h4 className="text-xl font-[400] text-black">{item.name}</h4>
+          <h4 className="text-l font-[300] text-black">{item.price}$</h4>
 
         </div>
+
+
+      </div>
+    )
+  }
+  return (
+    <>
+      <div className='flex justify-center p-8'>
+        <h1 className='text-black underline text-2xl  font-extrabold tracking-wide uppercase'>Products</h1>
+      </div>
+
+      <section
+
+        className="grid gap-4 lg:grid-cols-8  px-10 "
+        contentEditable="true" >
+
+        {PRODUCT.map(cardItem)}
       </section>
     </>
   )
--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
-const poduct=[
+export const PRODUCT=[
   {
     id:0,
     name:"GT2 Pro",
     brand:"huawei",
     dic:"k2ld2j",
-    img:"",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
     price:210,
     cat:"smartwatch"
   },
@@ -13,7 +13,7 @@ const poduct=[
     name:"GT2 Pro",
     brand:"huawei",
     dic:"k2ld2j",
-    img:"",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
     price:210,
     cat:"smartwatch"
   },
@@ -45,4 +45,203 @@ const poduct=[
     cat:"smartwatch"
   },
 
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
+     {
+    id:0,
+    name:"GT2 Pro",
+    brand:"huawei",
+    dic:"k2ld2j",
+    img:"https://images.dailyobjects.com/marche/product-images/1101/slick-phone-case-cover-for-iphone-13-images/Nimbus-Phone-Case-Cover-For-iPhone-13.png?tr=cm-pad_resize,v-2",
+    price:210,
+    cat:"smartwatch"
+  },
 ]
