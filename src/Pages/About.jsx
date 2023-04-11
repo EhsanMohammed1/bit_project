d898f9704a418e69aab271fd742f08560c135f0e
wecho12 <ehsankurd286@gmail.com>
2023-04-11 21:22:45 +0300

    complete a regester page design

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
                 type="text"
                 id="hero-field"
                 name="hero-field"
-                className="w-full bg-gray-100 bg-opacity-50 rounded border border-violet-300 focus:ring-2 focus:ring-violet-200 focus:bg-transparent focus:violet-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-full bg-gray-100 bg-opacity-50 rounded border border-violet-300 focus:ring-2  focus:bg-transparent focus:ring-violet-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <button className="inline-flex text-white bg-violet-500 border-0 py-2 px-6 focus:outline-none hover:bg-violet-600 rounded text-lg">
