8a29f0b98b176fd24c4ab26bce53e566ba687810
wecho12 <ehsankurd286@gmail.com>
2023-04-24 22:29:23 +0300

    reomve from card   button

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,114 +1,123 @@
+/* eslint-disable array-callback-return */
 import React from 'react'
-import { useSelector } from 'react-redux'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { decreasCard, removeFromCard } from '../Redux/CardSlice';
 const Checkout = () => {
+
   const card = useSelector((state) => state.card);
+  let total = 0;
+
+
+  const dispatch = useDispatch();
+  const handleRemoveCard = (cardItem) => {
+    dispatch(removeFromCard(cardItem))
+  }
+  const handleDecreseCard = (cardItem) => {
+    dispatch(decreasCard(cardItem))
+  }
+
+
+  card.cardItems?.map(cardItem => {
+    total = total + cardItem.price
+
+  }
+
+  )
+
 
   return (
 
-    <section className="h-full bg-gray-100 py-12 sm:py-16 lg:py-20">
+    <section className="h-full bg-gray-100  sm:py-16 lg:py-20">
+      <div className="flex items-center justify-center">
+        <h1 className="text-2xl font-semibold text-gray-900">Your Cart</h1>
+      </div>
+      {card.cardItems?.map((cardItem) => {
+        return <li className="mx-auto mt-4 max-w-6xl md:mt-12 bg-white shadow p-8 ">
+          <div key={cardItem.id} className="shrink-1 w-full px-12">
+            <img
+              className="h-32 w-32 max-w-full rounded-lg"
+              src={cardItem.img}
+              alt={cardItem.name}
+            />
+          </div>
+          <div className="relative flex flex-1 flex-col justify-between">
+            <div className="sm:col-gap-5 sm:grid sm:grid-cols-2 p-10">
+              <div className="pr-8 sm:pr-5 px-10">
+                <p className=" font-bold text-gray-900 text-2xl">
+                  {cardItem.name}
+                </p>
+                <p className="mx-0 mt-1 mb-0 text-gray-500 font-bold text-xl">
+                  {cardItem.brand}
+                </p>
+              </div>
+              <div className="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                <p className="shrink-0 w-32 text-3xl font-semibold text-gray-900 sm:order-2 sm:ml-8 sm:text-right  text-center">
+                  <span className='text-xl'>total </span>
+                  ${cardItem.price}
+                </p>
+
+                <div className="sm:order-1">
+                  <div className="mx-auto flex h-10 w-32 items-stretch text-gray-600 ">
+                    <button onClick={() => handleDecreseCard(cardItem)} className="flex items-center justify-center rounded-l-md bg-gray-200 px-4 transition hover:bg-violet-800 hover:text-white font-bold text-2xl">
+                      -
+                    </button>
+                    <div className="flex w-full items-center justify-center bg-gray-100 px-4  uppercase transition font-bold text-2xl">
+                      1
+                    </div>
+                    <button className="flex items-center justify-center rounded-r-md bg-gray-200 px-4 transition hover:bg-violet-800 hover:text-white font-bold text-2xl">
+                      +
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="absolute top-0 right-0 flex sm:bottom-0 sm:top-auto">
+              <button
+                type="button"
+                className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-violet-900"
+              >
+                <svg onClick={() => handleRemoveCard(cardItem)}
+                  className="block h-10 w-10 px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+
+                  />
+                </svg>
+              </button>
+            </div>
+
+            <p className="px-12 w-8 text-2xl font-semibold text-gray-900 sm:order-2 sm:ml-8 sm:text-right  text-center">
+              ${cardItem.price}
+            </p>
+
+          </div>
+
+        </li>
+      })
+      }
+
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-center">
-          <h1 className="text-2xl font-semibold text-gray-900">Your Cart</h1>
-        </div>
+
         <div className="mx-auto mt-8 max-w-6xl md:mt-12 ">
           <div className="bg-white shadow">
             <div className="px-4 py-6 sm:px-8 sm:py-10">
               <div className="flow-root">
-
-
-                {card.cardItems?.map(cardItem => (
-
-                  <li className="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
-                    <div key={cardItem.id} className="shrink-0">
-                      <img
-                        className="h-24 w-24 max-w-full rounded-lg object-cover"
-                        src={cardItem.img}
-                        alt={cardItem.name}
-                      />
-                    </div>
-                    <div className="relative flex flex-1 flex-col justify-between">
-                      <div className="sm:col-gap-5 sm:grid sm:grid-cols-2">
-                        <div className="pr-8 sm:pr-5">
-                          <p className="text-base font-semibold text-gray-900">
-                            {cardItem.name}
-                          </p>
-                          <p className="mx-0 mt-1 mb-0 text-sm text-gray-400">
-                            {cardItem.brand}
-                          </p>
-                        </div>
-                        <div className="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
-                          <p className="shrink-0 w-20 text-base font-semibold text-gray-900 sm:order-2 sm:ml-8 sm:text-right">
-                            ${cardItem.price}
-                          </p>
-                          <div className="sm:order-1">
-                            <div className="mx-auto flex h-8 items-stretch text-gray-600">
-                              <button className="flex items-center justify-center rounded-l-md bg-gray-200 px-4 transition hover:bg-black hover:text-white">
-                                -
-                              </button>
-                              <div className="flex w-full items-center justify-center bg-gray-100 px-4 text-xs uppercase transition">
-                                1
-                              </div>
-                              <button className="flex items-center justify-center rounded-r-md bg-gray-200 px-4 transition hover:bg-black hover:text-white">
-                                +
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="absolute top-0 right-0 flex sm:bottom-0 sm:top-auto">
-                        <button
-                          type="button"
-                          className="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900"
-                        >
-                          <svg
-                            className="block h-5 w-5"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M6 18L18 6M6 6l12 12"
-                              className=""
-                            />
-                          </svg>
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                ))};
-
-
-
-              </div>
-              <div className="mt-6 border-t border-b py-2">
-                <div className="flex items-center justify-between">
-                  <p className="text-sm text-gray-400">Subtotal</p>
-                  <p className="text-lg font-semibold text-gray-900">$399.00</p>
-                </div>
-                <div className="flex items-center justify-between">
-                  <p className="text-sm text-gray-400">Shipping</p>
-                  <p className="text-lg font-semibold text-gray-900">$8.00</p>
-                </div>
               </div>
               <div className="mt-6 flex items-center justify-between">
-                <p className="text-sm font-medium text-gray-900">Total</p>
-                <p className="text-2xl font-semibold text-gray-900">
-                  <span className="text-xs font-normal text-gray-400">USD</span>{" "}
-                  408.00
+                <p className="text-2xl font-medium text-gray-900">Total</p>
+                <p className="text-3xl font-semibold text-gray-900">
+                  <span className="text-xl font-normal text-gray-500">USD</span>{" "}
+                  {total}
                 </p>
               </div>
-
-
-
-
-
-
-
 
               <div className="mt-6 text-center">
                 <button
--- src/Redux/CardSlice.js
+++ src/Redux/CardSlice.js
@@ -31,8 +31,48 @@ const cardSlice = createSlice({
       }
       localStorage.setItem("cardItems", JSON.stringify(state.cardItems));
     },
+
+    removeFromCard(state, action) {
+      const nextCardItems = state.cardItems.filter(
+        (cardItem) => cardItem.id !== action.payload.id
+      );
+      state.cardItems = nextCardItems;
+
+      localStorage.setItem("cardItems", JSON.stringify(state.cardItems));
+      toast.error(`${action.payload.name}is Romved from Card`, {
+        position: "top-left",
+      });
+    },
+
+
+    decreasCard(state, action) {
+      const itemIndex = (state = state.cardItems.findIndex(
+        (cardItem) => cardItem.id === action.payload.id
+      ));
+
+      if (state.cardItems[itemIndex].cardQuantity > 1) {
+        state.cardItems[itemIndex].cardQuantity -= 1;
+
+        toast.info(`${action.payload.name}cardQuantity`, {
+          position: "top-left",
+        });
+      } 
+      
+      
+      else if (state.cardItems[itemIndex].cardQuantity === 1) {
+        const nextCardItems = state.cardItems.filter(
+          (cardItem) => cardItem.id !== action.payload.id
+        );
+        state.cardItems = nextCardItems;
+
+        toast.error(`${action.payload.name}is Romved from Card`, {
+          position: "top-left",
+        });
+      }
+      localStorage.setItem("cardItems", JSON.stringify(state.cardItems));
+    },
   },
 });
 
-export const { addToCard } = cardSlice.actions;
+export const { addToCard, removeFromCard, decreasCard } = cardSlice.actions;
 export default cardSlice.reducer;
